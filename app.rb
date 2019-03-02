4e7c8dbfe8a1e16713bd4a3a87579a32c51dbbd1
jonmackeyparty <jon@jonmackey.party>
2019-03-02 13:30:54 -0500

    add session, form, controller action

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session.merge!(params)
+
+    erb :checkout
+  end
+
+
 end
