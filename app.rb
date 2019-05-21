9b79b772a4e8eaec629cc098f277f5aa899655e4
Alison Reilly <reillyamr@gmail.com>
2019-05-21 16:03:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index 
+  end 
+  
+  # get '/checkout' do 
+  #   @session = session
+  #   session["item"] = @items
+  # end
+  
+  post '/checkout' do 
+    @items = params[:item]
+    @session = session
+    session["item"] = @items
+    
+  end
+  
+  configure do 
+    enable :sessions
+    set :session_secret, "Graycie" #find sinatra documentaion for setting this to a secure random #number generator value and then sharing that secret via environment variables in the shell to 
+      #sinatra
+  end 
 end
