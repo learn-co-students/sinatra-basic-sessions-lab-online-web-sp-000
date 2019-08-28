0bbfadbd1e04bb5a382099311087f8a1af91e9d9
Peter Grant <jgrantps@hotmail.com>
2019-08-28 11:47:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[params.keys.first] = params.values.first
+    @item = Items.new(params)
+
+    #  binding.pry
+    erb :checkout
+
+  end
+
+
 end
