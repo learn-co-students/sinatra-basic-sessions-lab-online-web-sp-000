56fec63253da52911f1c18ff448a69dfcdef50b5
Lauren McCoy <lmccoy8911@gmail.com>
2019-09-09 22:30:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
-
+require_relative './config/environment'
+require 'pry'
 class App < Sinatra::Base
+  
+  enable :sessions 
+  set :session_secret, "secret"
+  
+  get '/' do 
+    erb :index 
+  end 
+
+  post '/checkout' do 
+    session[:item] = params[:item] 
+  end 
+
 end
