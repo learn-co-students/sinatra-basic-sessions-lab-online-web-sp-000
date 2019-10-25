72f9c665a33c17ec32f63284f4010c00e8e3cfda
Bernadette Davis <bsd231@nyu.edu>
2019-10-25 18:38:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "not_a_secret"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  post '/checkout' do
+    session.name = "name"
+    @session = session
+    params[:item]
+
+  end
+
 end
