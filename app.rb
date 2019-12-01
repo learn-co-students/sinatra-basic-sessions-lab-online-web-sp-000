f681afa2ae249e327d39c51cf2d2cd2d43e95050
Lukie Kang <Lukie Kang>
2019-12-01 20:27:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
+
+
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "not_a_secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @session = session
+        @session["item"] = params[:item]
+        binding.pry
+        erb :checkout
+    end 
+
 end
