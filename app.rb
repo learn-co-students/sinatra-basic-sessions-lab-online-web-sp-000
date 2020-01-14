809364bae7d8ab748ff82103024101a36513b1c5
Connor Cowan <ccowan003@comcast.net>
2020-01-14 15:21:29 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  GET '/' do
-    
+  configure do
+    enable :sessions
+    set :session_secret, "flatiron"
+  end
+
+  get '/' do
+    erb :index          #renders a form "index"
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]          #sets params in the session hash
+    @session = session                  #session hash stored in instance variable, that we can access in views
+    erb :checkout
   end
 
 
