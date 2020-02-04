57e25373aead826d87ac9e6ba5590f0020183cb4
Anthony Tilelli <anthony@tilelli.me>
2020-02-04 17:59:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "ab1a28c6d30aa89b699ad1b4fe86f64e05b791163e9580e12d2aabfd9ac7ed7"
+  end
+  
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do 
+    session['item'] = params['item']
+    @session = session
+    erb :result
+  end
 end
