60688ac372a9dc2f08ad9db19aa484daca8a3843
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-23 22:58:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @sessions = sessions
+    item = params["item"]
+    @sessions[:item] = item
+  end
 end
