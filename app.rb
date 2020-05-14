86f8fc4da4c771e008e9f7c40e54e2c149d827c0
marcon29 <online@markstabler.com>
2020-05-13 18:30:05 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions        
+        set :session_secret, "cart"
+    end
+
+    get '/' do
+        @session = session
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]        
+        @session = session        
+        redirect to '/checkout'
+    end
+
+    get '/checkout' do
+        @session = session
+        erb :show
+    end
+
 end
