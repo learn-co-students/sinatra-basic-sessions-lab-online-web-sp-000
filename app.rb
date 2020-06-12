e5c833ee2b2ea42231b1657d9d414e5a709d71f3
Taishawnk <Taishawnking1@yahoo.com>
2020-06-11 19:30:20 -0600

    Done.

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     end
     post '/checkout' do 
         @session= session
-        session[iteam]= "boots"
+        session[item]= "boots"
         binding.pry
     end
 end
