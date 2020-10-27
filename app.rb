5155712b7a5d5818e7be3f24f53272592226af3e
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-27 01:05:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :sessions_secret, "kVTqGDxlODlw1GMhhNfZufXIFajnFKcnNQkRCDEGSLzDg111B8Yel3myYk8yAhZ0"
+  end 
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+  end
+  
 end
