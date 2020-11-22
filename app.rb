673b0e391a4921cabdc9b2f827f000ab91294f35
DabooUA <sbardysh@tutanota.com>
2020-11-22 11:10:15 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, 'd94b83161b215c7d52100479f37f1d4e28977576fab1ea2f37d5ba3cb24f99afab8ae2d209ec7bd5499c34408bb54528626b09c16d7292eebab22a1a162f0ce8'
+    end
+    
+    
+    
+    get "/" do
+        erb :index
+    end
+
+    post "/checkout" do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
 end
