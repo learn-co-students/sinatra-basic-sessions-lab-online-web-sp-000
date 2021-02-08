6fda00a1e899e73c084b1cdecee4f77bb201e553
Evan Burton <evan@heyevan.com>
2021-02-08 11:14:29 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
+require 'securerandom'
+
+configure do 
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') {SecureRandom.hex(64)}
+end 
+
+
+
 
 class App < Sinatra::Base
+
+    get '/' do
+
+    erb :index 
+    end 
+
+    post '/checkout' do 
+        @session = session 
+        item = params["item"]
+        @session[:item] = item 
+        erb :checkout
+        raise params.inspect
+    end 
+
 end
