2d4235550adf12d651dc8cc22f06339f7b20321c
GoldenPavilion <rcorystine@gmail.com>
2021-03-22 17:23:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, ENV.fetch('SESSION-SECRET') {SecureRandom.hex(64)}
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @session = session
+        item = params[:item]
+        @session[:item] = item
+    end
+
 end
