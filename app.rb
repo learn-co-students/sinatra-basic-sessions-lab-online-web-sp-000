318bd6293944c46a12b49a717e72889614b7a923
Jeff Bai <j.bai@wustl.edu>
2021-04-12 11:36:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+  end
 end
